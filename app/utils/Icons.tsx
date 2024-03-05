2c16e7cc7f0a8fb526bf3f3ee46c1363d88a4883
DanyRanjbar <danyranjbar@gmail.com>
2024-03-05 20:27:34 +0330

    init commit

---
+++ app/utils/Icons.tsx
@@ -0,0 +1,41 @@
+import {
+  CalendarDays,
+  CloudDrizzleIcon,
+  CloudRain,
+  CloudSun,
+  Cloudy,
+  CommandIcon,
+  Droplet,
+  Eye,
+  Gauge,
+  Github,
+  Navigation,
+  Search,
+  Snowflake,
+  SunDim,
+  Sunset,
+  Thermometer,
+  ThermometerSun,
+  UserRound,
+  Wind,
+} from "lucide-react";
+
+export const github = <Github size={15} />;
+export const commandIcon = <CommandIcon size={20} />;
+export const searchIcon = <Search />;
+export const drizzleIcon = <CloudDrizzleIcon size={35} />;
+export const rain = <CloudRain size={30} />;
+export const snow = <Snowflake size={30} />;
+export const clearSky = <CloudSun size={30} />;
+export const cloudy = <Cloudy size={30} />;
+export const navigation = <Navigation size={15} />;
+export const thermo = <ThermometerSun size={15} />;
+export const sunset = <Sunset size={15} />;
+export const wind = <Wind size={15} />;
+export const gauge = <Gauge size={15} />;
+export const droplet = <Droplet size={15} />;
+export const thermometr = <Thermometer size={15} />;
+export const eye = <Eye size={15} />;
+export const people = <UserRound size={15} />;
+export const clander = <CalendarDays size={15} />;
+export const sun = <SunDim size={15} />;
